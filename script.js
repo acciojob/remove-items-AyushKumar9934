e94470fbf1d47b036803a92f1b8b6a9cea57c9b6
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-04-11 12:13:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let x=document.getElementById("colorSelect");
+
+
+function change(){
+    let i=x.selectedIndex;
+    x.options.remove(i);
+    console.log(i);
+}
+document.getElementById("btn").addEventListener("click",change);
